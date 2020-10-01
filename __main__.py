3ec2e563dea90c7e686a7a0aa44a21e6320d112f
Shivam <shivamsn97@gmail.com>
2020-10-01 18:43:42 +0530

    Initial Release

---
+++ __main__.py
@@ -0,0 +1,99 @@
+import json
+import inquirer
+from inquirer import errors
+
+data = {}
+with open("data.json",'r') as jsfile:
+    data = json.loads(jsfile.read())
+
+def int_validator(answers, current):
+    if not current.strip():
+        raise errors.ValidationError('', reason='Please provide a rank!')
+    try:
+        int(current)
+        return True
+    except ValueError:
+        raise errors.ValidationError('', reason='Provided value is not an integer!')
+
+questions = [
+    inquirer.Text('college',
+                  message='Keywords to find in college name (Leave blank for all)?',
+                  default=''),
+    inquirer.Checkbox('quota',
+                  message='Quota? (Use space to deselect, X = selected, O = not selected)',
+                  choices=data["values"]["quota"],
+                  default=["AI", "OS"]),
+    inquirer.Checkbox('category',
+                      message='Category?',
+                      choices=data["values"]["category"],
+                      default=[x for x in data["values"]["category"] if ("gen" in x.lower() or "obc" in x.lower()) and "pwd" not in x.lower()]
+                      ),
+    inquirer.Checkbox('seat_pool',
+                      message='Seat Pool?',
+                      choices=data["values"]["seat_pool"],
+                      default=["Gender-Neutral"]
+                      ),
+    inquirer.Text('streams',
+                      message='Keywords to find in stream (Leave blank for all)?',
+                      default=""
+                      ),
+    inquirer.Text(
+        "rank",
+        message="Tell me your rank",
+        validate=int_validator
+    )
+]
+
+answers = inquirer.prompt(questions)
+
+predict = []
+for i in data["data"]:
+    if(
+        answers["college"].lower() in i["college"].lower() and
+        i["quota"] in answers["quota"] and
+        i["category"] in answers["category"] and
+        i["seat_pool"] in answers["seat_pool"] and
+        answers["streams"].lower() in i["stream"].lower() and
+        i["closing_rank"] >= int(answers["rank"])
+    ):
+        predict.append(i)
+
+import time
+if not predict:
+    print("No college found. Exiting in 2 seconds")
+    time.sleep(2)
+    exit()
+
+predict.sort(key=lambda x: x["closing_rank"])
+
+for i in predict:
+    print(f"""----------------------------------------------------------
+Name: {i["college"]}
+Stream: {i["stream"]}
+Round: {i["round"]}
+Quota: {i["quota"]}
+Category: {i["category"]}
+Seat Pool: {i["seat_pool"]}
+Opening Rank: {i["opening_rank"]}
+Closing Rank: {i["closing_rank"]}
+    """)
+
+
+if inquirer.list_input("Save to file?",choices=['Yes', 'No']) == "Yes":
+    fl_name = inquirer.text(message="Enter file name.")
+    if not fl_name.endswith(".txt"): fl_name+=".txt"
+    with open(fl_name, "w") as file:
+        for i in predict:
+            file.write(f"""----------------------------------------------------------
+Name: {i["college"]}
+Stream: {i["stream"]}
+Round: {i["round"]}
+Quota: {i["quota"]}
+Category: {i["category"]}
+Seat Pool: {i["seat_pool"]}
+Opening Rank: {i["opening_rank"]}
+Closing Rank: {i["closing_rank"]}
+    """)
+    print("Done! Exiting in 2 seconds.")
+    time.sleep(2)
+    exit()
---
+++ convert-pdfs.py
@@ -0,0 +1,62 @@
+import tabula
+import glob, os
+import os.path
+import json
+
+files = glob.glob("pdfs/*.pdf")
+
+whole_data = []
+all_colleges = []
+all_categories = []
+all_quotas = []
+all_seat_pools = []
+all_streams = []
+
+
+for i in files:
+    inputf = os.path.join(os.path.dirname(os.path.realpath(__file__)), i)
+    output = os.path.join(os.path.dirname(os.path.realpath(__file__)) ,"pdfs-to-jsons" + os.sep + ".".join(i.split(os.sep)[-1].split(".")[:-1]) + ".csv")
+    print("Reading {}. ".format(i.split(os.sep)[-1]))
+    df = tabula.read_pdf(inputf, pages='all')
+    #data = []
+    for i in df:
+        page = i.dropna().values
+        for row in page:
+            if str(row[0]) not in ['1','2','3','4','5','6','7']:
+                continue
+            temp_dict = {
+                "round": int(row[0]),
+                "college": row[1].replace("\r", " "),
+                "stream": row[2].replace("\r", " "),
+                "quota": row[3].replace("\r", " "),
+                "category": row[4].replace("\r", " "),
+                "seat_pool": row[5].replace("\r", " "),
+                "opening_rank": int(float(row[6][:-1])) if str(row[6]).endswith("P") else int(float(row[6])),
+                "closing_rank": int(float(row[7][:-1])) if str(row[7]).endswith("P") else int(float(row[7])),
+                "rank_type": "preparatory" if str(row[7]).endswith("P") else "general" if row[4] == "General" else "category"
+            }
+            if temp_dict["college"] not in all_colleges:
+                all_colleges.append(temp_dict["college"])
+            if temp_dict["stream"] not in all_streams:
+                all_streams.append(temp_dict["stream"])
+            if temp_dict["category"] not in all_categories:
+                all_categories.append(temp_dict["category"])
+            if temp_dict["quota"] not in all_quotas:
+                all_quotas.append(temp_dict["quota"])
+            if temp_dict["seat_pool"] not in all_seat_pools:
+                all_seat_pools.append(temp_dict["seat_pool"])
+            whole_data.append(temp_dict)
+
+print("Writing to data.json")
+with open("data.json", "w") as output:
+    json.dump({
+        "values" : {
+            "round": [1,2,3,4,5,6,7],
+            "college": all_colleges,
+            "stream": all_streams,
+            "category": all_categories,
+            "quota": all_quotas,
+            "seat_pool": all_seat_pools
+        },
+        "data" : whole_data
+    }, output, indent=4)
